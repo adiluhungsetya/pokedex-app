8f167ae77eec86fae8a8d2f3ac9e89feca6ee03a
Adiluhung Setya P <adiluhung410@gmail.com>
2023-06-26 10:12:06 +0700

    [ADD] Create constant value

---
+++ src/constant/common.ts
@@ -0,0 +1,13 @@
+export const BASE_URL : string = "https://pokeapi.co/api/v2"
+export const BASE_IMG_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other";
+
+export const statsMaxValues : {[key : string] : number} = {
+    hp: 714,
+    attack: 714,
+    defense: 614,
+    "special-attack": 504,
+    "special-defense": 614,
+    speed: 504,
+}
+
+export const maxData : number = 1281;
