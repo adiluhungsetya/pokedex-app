327560af3631579398aab34cf1e604eb2453887b
Adiluhung Setya P <adiluhung410@gmail.com>
2023-06-26 10:11:23 +0700

    [ADD] Create custom hook for fetching list of pokemon data

---
+++ src/hooks/usePokemons.ts
@@ -0,0 +1,44 @@
+import { formatPokemonData } from './../utils/helper';
+import { apiFetch } from "../utils/api-fetch";
+import { useQuery } from 'react-query';
+import { BasePokemon, Pokemon, PokemonResponse, DefaultResponse, Base } from '../types/pokemon-types';
+
+const usePokemons = (type : string, limit: number, prevData: Pokemon[] ) => {
+    const { data } = useQuery({
+        queryKey: ['pokemons', {type, limit}],
+        queryFn: async () => {
+
+            if(type === "all"){
+                const { results: pokemonList } : DefaultResponse = await apiFetch(`/pokemon?offset=${limit}&limit=30`);
+
+                const pokemons = await Promise.all<Pokemon>(
+                    pokemonList.map(async (pokemon : Base) => {
+                        const res = await fetch(pokemon.url);
+                        const data : PokemonResponse = await res.json();
+    
+                        return formatPokemonData(data);
+                    })
+                );
+
+                return [...prevData, ...pokemons];
+            }
+
+            const { pokemon: pokemonList } : {pokemon : BasePokemon[]} = await apiFetch(`/type/${ type }`);
+            
+            const pokemons = await Promise.all<Pokemon>(
+                pokemonList.map(async ({ pokemon } : BasePokemon) => {
+                    const res = await fetch(pokemon.url);
+                    const data : PokemonResponse = await res.json();
+
+                    return formatPokemonData(data);
+                })
+            );
+
+            return pokemons;
+        }
+    });
+
+    return data;
+};
+
+export default usePokemons;
